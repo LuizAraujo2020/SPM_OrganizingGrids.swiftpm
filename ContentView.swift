022f0804db74073e9a1034b4ee5d81167265f879
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2022-04-12 13:20:53 -0300

    Creating a Grid - Step 2

--- ContentView.swift
+++ ContentView.swift
@@ -1,6 +1,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columnLayout = Array(repeating: GridItem(), count: 3)
+    
+    @State private var selectedColor = Color.gray
+    
+    let allColors: [Color] = [
+        .pink,
+        .red,
+        .orange,
+        .yellow,
+        .green,
+        .mint,
+        .teal,
+        .cyan,
+        .blue,
+        .indigo,
+        .purple,
+        .brown,
+        .gray
+    ]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
