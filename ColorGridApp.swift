0f9f38d3ef41c5e78a94a38d51f391cf3ae5eee6
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2022-04-12 13:13:41 -0300

    Configuring the App -  Step 2

--- ColorGridApp.swift
+++ ColorGridApp.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-@main
+@main // The @main make this the entry point of the program — the code the app starts running first.
 struct ColorGridApp: App {
     var body: some Scene {
         WindowGroup {
